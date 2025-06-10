b8a01ff19a80d331f7ead937fe9d468a6bf75fde
Asish Jose <asishjosekakkattil@gmail.com>
2025-06-10 17:19:55 +0530

    Static files management

--- Django/projects/movie_manager/movies/views.py
+++ Django/projects/movie_manager/movies/views.py
@@ -4,10 +4,35 @@ from django.shortcuts import render
 # Create your views here.
 
 def create(request):
+    if request.POST:
+        #print(request.POST)
+        print(request.POST.get('year'))
     return render(request, 'create.html')
 
 def list(request):
-    return render(request, 'list.html')
+    movie_data = {
+        'movies':[ {
+        'title':'Godfather',
+        'year': 1990,
+        'summary':'stroy of an underworld don',
+        'success':True,
+        'img': 'godfather.jpeg'
+    },
+    {
+        'title':'Goat Life',
+        'year': 1990,
+        'success':False,
+        'img': 'goatlife.jpeg'
+    },
+    {
+        'title':'Empuraan',
+        'year': 1990,
+        'summary':'stroy of an underworld don',
+        'success':False,
+        'img': 'empuran.jpeg'
+    }
+        ]}
+    return render(request, 'list.html', movie_data)
 
 
 def edit(request):
