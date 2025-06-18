d4bcca7a478f1ee32fa609c66d7077be0f722c96
Asish Jose <asishjosekakkattil@gmail.com>
2025-06-18 20:31:23 +0530

    familia for CRUD

--- Django/practise/familia/family/models.py
+++ Django/practise/familia/family/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class FamilyMember(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
--- Django/practise/familia/family/views.py
+++ Django/practise/familia/family/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . models import FamilyMember
 # Create your views here.
+
+def home(request):
+    return render(request, 'family/home.html')
+
+def list(request):
+    members = FamilyMember.objects.all()
+    return render(request, 'family/list.html', {'members':members})
+
+def create(request):
+    if request.method=='POST':
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+
+        FamilyMember.objects.create(
+            name=name,
+            age=age
+        )
+        return redirect('list')
+    return render(request, 'family/create.html')
+
+def delete(request, pk):
+    instance = FamilyMember.objects.get(pk=pk)
+    instance.delete()
+    return redirect('list')
+
+def edit(request,pk):
+    instance = FamilyMember.objects.get(pk=pk)
+
+    if request.method=='POST':
+        instance.name = request.POST.get('name',instance.name)
+        instance.age = request.POST.get('age',instance.age)
+        instance.save()
+        return redirect('list')
+
+    return render(request, 'family/edit.html',{'member':instance})
