839154fed0d68aa76dd280e643e04a6642ec93aa
Asish Jose <asishjosekakkattil@gmail.com>
2025-06-23 20:06:03 +0530

    sessions and cookies

--- Django/projects/movie_manager/movies/views.py
+++ Django/projects/movie_manager/movies/views.py
@@ -27,26 +27,55 @@ def create(request):
     return render(request, 'create.html',{'frm':frm})
 
 def list(request):
-    movie_set=MovieInfo.objects.all()
-    print(movie_set)
-    return render(request, 'list.html', {'movies':movie_set })
+    #print(request.session)
+    #print(request.COOKIES)
+
+    ##visits counts using cookies.........
+    # visits = int(request.COOKIES.get('visits',0))
+    # visits=visits+1
+    # movie_set=MovieInfo.objects.all()
+
+    ##Recent Visits.........
+    recent_visits=request.session.get('recent_visits',[])
+
+    ##visits counts usings sessions..........
+    count = request.session.get('count',0)
+    count = int(count)
+    count = count+1
+    request.session['count']=count
+    ##Recent Visits................
+    recent_movie_set = MovieInfo.objects.filter(pk__in=recent_visits)
+
+    ##ORM Queries.........
+    #movie_set=MovieInfo.objects.filter(year=2023,title='Jailer')
+    #movie_set=MovieInfo.objects.exclude(year=2023).order_by('-year')
+    #movie_set = MovieInfo.objects.filter(year__gt=2023)
+    #movie_set = MovieInfo.objects.filter(actors__name='Mohanlal')
+    movie_set = MovieInfo.objects.all()
+
+    response = render(request, 'list.html', {
+        'recent_movies':recent_movie_set,
+        'movies':movie_set,'visits':count })
+    #response.set_cookie('visits',visits)
+    return response
 
 
 def edit(request,pk):
     instance_to_be_edited=MovieInfo.objects.get(pk=pk)
     if request.POST:
         # title=request.POST.get('title')
-        # year=request.POST.get('year')
-        # description=request.POST.get('description')
         # instance_to_be_edited.title=title
-        # instance_to_be_edited.year=year
-        # instance_to_be_edited.description=description
         # instance_to_be_edited.save()
         frm=MovieForm(request.POST,request.FILES, instance=instance_to_be_edited)
         if frm.is_valid():
             instance_to_be_edited.save()
             return redirect('list')
     else:
+        #recent visits.............
+        recent_visits=request.session.get('recent_visits',[])
+        recent_visits.insert(0,pk)
+        request.session['recent_visits']=recent_visits
+
         frm=MovieForm(instance=instance_to_be_edited)
         
     frm=MovieForm(instance=instance_to_be_edited)
